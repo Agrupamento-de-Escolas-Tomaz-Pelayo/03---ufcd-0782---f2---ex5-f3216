bea8092bfc9647a541869fdcaa1a90d529cb9d4e
f3216 <122741727+f3216@users.noreply.github.com>
2023-02-01 17:00:56 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo "A executar os testes ..."
 echo
 
-output=$(./a.out)
+program_output=$(./a.out)
 expected_output_1="##############"
 expected_output_2="# Ola Mundo! #"
 expected_output_3="##############"
